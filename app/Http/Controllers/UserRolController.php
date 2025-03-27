6b6b02d4ad65881bb5308975f71aa2945e1a2cdb
john mario chaverra <chaverradvox@gmail.com>
2025-03-27 01:14:17 -0500

    se realizo el controlador a user rol

--- app/Http/Controllers/UserRolController.php
+++ app/Http/Controllers/UserRolController.php
@@ -2,22 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\User_rol;
+use App\Models\UserRol;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class UserRolController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+    
+    private $response = [
+        'status' => 'ok',
+        'message' => null,
+        'data' => []
+    ];
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    public function index()//consultar en bloque ofiltro,get sin nada en la url , talvez body
+    {
+        $query = UserRol::query();
+
+
+
+
+
+    $this->response['data'] = $query->get();
+    return response()->json($this->response, 2000);
+    }
     public function create()
     {
         //
@@ -28,38 +38,105 @@ class UserRolController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $UserRol = UserRol::create([
+                'name' => $request['name'],
+                'rol_id' => $request['rol_id'],
+
+            ]);
+
+            if (!$UserRol) {
+                $this->response['status'] = 'error';
+                $this->response['message'] = 'no se creo el tipo de Rol';
+            }
+
+        } catch(QueryException $e){
+            return response()->json($this->response, 500);
+        }
+
+        $this->response['data'] = $UserRol;
+        return response()->json($this->response, 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(User_rol $user_rol)
+    public function show($id) 
     {
-        //
+        $UserRol = UserRol::find($id);
+        if(!$UserRol)
+        {
+            $this->response['status'] = 'error';
+            $this->response['message'] = 'no se encontró  el tipo de usuario  rol ';
+            return response()->json($this->response, 400);
+        }
+
+        $this->response['data'] = $UserRol;
+        return response()->json($this->response, 200);
     }
+
+    
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User_rol $user_rol)
+    public function edit(UserRol $UserRol)
     {
-        //
+        
+    
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User_rol $user_rol)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $UserRol = $UserRol::find($id);
+
+            if(!$UserRol) {
+                $this->response['status'] = 'error';
+                $this->response['message'] = 'no se encontro el tipo usuario';
+                return response()->json($this->response, 400);
+
+
+            }
+            $UserRol->update([
+                'name' => $request['name'],
+                'rol_id'=>$request['rol_id'],
+            ]);
+            
+           
+		} catch (QueryException $e) {
+			return $this->response['message'] = $e->getMessage();
+		}
+
+        $this->response['data'] = $UserRol;
+
+        return response()->json($this->response, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User_rol $user_rol)
+    public function destroy($id) //post , recibe solo id en la url
     {
-        //
+        $UserRol = UserRol::find($id);
+
+        if(!$UserRol){
+            $this->response['status'] = 'error';
+            $this->response['message'] = 'No se ha encontrado el Tipo de identificacion';
+            return response()->json($this->response, 400);
+        }
+
+        if($UserRol->delete()){
+            $this->response['data'] = $UserRol;
+            return response()->json($this->response, 200);
+        }
+        else{
+            $this->response['status'] = 'error';
+            $this->response['message'] = 'No se ha eliminado el Tipo de identificacion';
+            return response()->json($this->response, 400);
+        }
     }
 }
